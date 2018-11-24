13df77226ffa771c1357d67d9daa9f5cc82b085b
NostosP <paruta.info@gmail.com>
2018-11-24 17:59:09 +0100

    Add dialogflow webhook response

--- index.js
+++ index.js
@@ -26,10 +26,9 @@ server.post('/get-movie-details', (req, res) => {
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
-            return res.json({
-                speech: dataToSend,
-                displayText: dataToSend,
-                source: 'get-movie-details'
+            return res.json({                
+                "fulfillmentText": dataToSend,
+                "source": "https://safe-journey-43214.herokuapp.com/get-movie-details",                
             });
         });
     }, (error) => {
