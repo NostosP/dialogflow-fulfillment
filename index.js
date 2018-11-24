341ff2d8c5eb4c1b883e821352c63269e710646d
NostosP <paruta.info@gmail.com>
2018-11-24 18:42:29 +0100

    Delete http code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const http = require('http');
 var rp = require('request-promise');
 const API_KEY = require('./apiKey');
 
@@ -16,53 +15,35 @@ server.use(bodyParser.json());
 
 server.post('/get-movie-details', (req, res) => {
 
-    // const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
     const movieToSearch = req.body.queryResult.parameters.movie;
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     var options = {
-        "method": "GET",
-        "uri": reqUrl,
-        "json": true, // Automatically parses the JSON string in the response
-        "headers": {
-            "User-Agent": "My little demo app"
-        }
+        method: "GET",
+        uri: reqUrl,
+        json: true, // Automatically parses the JSON string in the response
     };
     rp(options).
         then(function (result){
-            let dataToSend = '';
-            dataToSend = `${result.Title} is a ${result.Actors} starer ${result.Genre} movie, released in ${result.Year}. It was directed by ${result.Director}`;
-            return res.json({                
-                "fulfillmentText": dataToSend,
-                "source": "https://safe-journey-43214.herokuapp.com/get-movie-details",                
+            if (result.Response == 'True') {
+                let dataToSend = '';
+                dataToSend = `${result.Title} is a ${result.Actors} starer ${result.Genre} movie, released in ${result.Year}. It was directed by ${result.Director}`;
+                return res.json({                
+                    "fulfillmentText": dataToSend,
+                    "source": "https://safe-journey-43214.herokuapp.com/get-movie-details"               
+                });
+            } else return res.json({                
+                "fulfillmentText": "Sorry, couldn't find your movie",
+                "source": "https://safe-journey-43214.herokuapp.com/get-movie-details"               
             });
+            
         })
         .catch(function (err){
             return res.json({
-                "fulfillmentText": "Something went wrong!"
+                "fulfillmentText": "Something went wrong!",
+                "source": "https://safe-journey-43214.herokuapp.com/get-movie-details"
             })
         });
-    // http.get(reqUrl, (responseFromAPI) => {
-    //     let completeResponse = '';
-    //     responseFromAPI.on('data', (chunk) => {
-    //         completeResponse += chunk;
-    //     });
-    //     responseFromAPI.on('end', () => {
-    //         const movie = JSON.parse(completeResponse);
-    //         let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-    //         dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
-
-    //         return res.json({                
-    //             "fulfillmentText": dataToSend,
-    //             "source": "https://safe-journey-43214.herokuapp.com/get-movie-details",                
-    //         });
-    //     });
-    // }, (error) => {
-    //     return res.json({
-    //         speech: 'Something went wrong!',
-    //         displayText: 'Something went wrong!',
-    //         source: 'get-movie-details'
-    //     });
-    // });
+    
 });
 
 server.listen((process.env.PORT || 8000), () => {
