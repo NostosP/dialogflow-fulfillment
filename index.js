25ca95a8cee038c16459eb1625cd1551cf67a66a
NostosP <paruta.info@gmail.com>
2018-11-24 18:20:33 +0100

    Trying request instead of http

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const http = require('http');
+// const http = require('http');
+var rp = require('request-promise');
 const API_KEY = require('./apiKey');
 
 const server = express();
+
 server.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -17,13 +19,19 @@ server.post('/get-movie-details', (req, res) => {
     // const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
     const movieToSearch = req.body.queryResult.parameters.movie;
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
-    http.get(reqUrl, (responseFromAPI) => {
-        let completeResponse = '';
-        responseFromAPI.on('data', (chunk) => {
-            completeResponse += chunk;
-        });
-        responseFromAPI.on('end', () => {
-            const movie = JSON.parse(completeResponse);
+    var options = {
+        uri: reqUrl,
+        qs: {
+            access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
+        },
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    rp(options).
+        then(function (repos){
+            const movie = JSON.parse(repos);
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
@@ -31,14 +39,34 @@ server.post('/get-movie-details', (req, res) => {
                 "fulfillmentText": dataToSend,
                 "source": "https://safe-journey-43214.herokuapp.com/get-movie-details",                
             });
+        })
+        .catch(function (err){
+            return res.json({
+                "fulfillmentText": "Something went wrong!"
+            })
         });
-    }, (error) => {
-        return res.json({
-            speech: 'Something went wrong!',
-            displayText: 'Something went wrong!',
-            source: 'get-movie-details'
-        });
-    });
+    // http.get(reqUrl, (responseFromAPI) => {
+    //     let completeResponse = '';
+    //     responseFromAPI.on('data', (chunk) => {
+    //         completeResponse += chunk;
+    //     });
+    //     responseFromAPI.on('end', () => {
+    //         const movie = JSON.parse(completeResponse);
+    //         let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+    //         dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+
+    //         return res.json({                
+    //             "fulfillmentText": dataToSend,
+    //             "source": "https://safe-journey-43214.herokuapp.com/get-movie-details",                
+    //         });
+    //     });
+    // }, (error) => {
+    //     return res.json({
+    //         speech: 'Something went wrong!',
+    //         displayText: 'Something went wrong!',
+    //         source: 'get-movie-details'
+    //     });
+    // });
 });
 
 server.listen((process.env.PORT || 8000), () => {
