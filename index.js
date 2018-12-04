7ed2da8dd1cdca8b0002acbb85ef9a66a48f9e41
NostosP <paruta.info@gmail.com>
2018-12-04 09:57:55 +0100

    Works with ids

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var rp = require('request-promise');
-const API_KEY = require('./apiKey');
 
 const server = express();
 
@@ -15,39 +14,29 @@ server.use(bodyParser.json());
 
 server.post('/', (req, res) => {
 
-    var options = {
-        method: "GET",
-        uri: reqUrl,
-        json: true, // Automatically parses the JSON string in the response
-    };
-
     if (req.body.queryResult.intent.displayName == "Find Player Intent - Get Name") {
         const playerToSearch = req.body.queryResult.parameters.player;
-        const reqUrl = encodeURI(`http://localhost:8080/provaTesi/webapi/players/${playerToSearch}`);
-        console.log(reqUrl);
-    }
-    
-    
+        let reqUrl = encodeURI(`http://localhost:8080/provaTesi/webapi/players/${playerToSearch}`);        
+        var options = {
+            method: "GET",
+            uri: reqUrl,
+            json: true, // Automatically parses the JSON string in the response
+        };
+    }    
     
     rp(options).
         then(function (result){
-            if (result.Response == 'True') {
-                let dataToSend = '';
-                dataToSend = `${result.firstName} ${result.lastName} is ${result.age} years old`;
-                return res.json({                
-                    "fulfillmentText": dataToSend,
-                    "source": "https://safe-journey-43214.herokuapp.com/get-movie-details"               
-                });
-            } else return res.json({                
-                "fulfillmentText": "Sorry, I couldn't find your player",
-                "source": "https://safe-journey-43214.herokuapp.com/get-movie-details"               
-            });
-            
+            let dataToSend = '';
+            dataToSend = `${result.firstName} ${result.lastName} is ${result.age} years old`;
+            return res.json({                
+                "fulfillmentText": dataToSend,
+                "source": "https://safe-journey-43214.herokuapp.com"               
+            });            
         })
         .catch(function (err){
             return res.json({
                 "fulfillmentText": "Something went wrong!",
-                "source": "https://safe-journey-43214.herokuapp.com/get-movie-details"
+                "source": "https://safe-journey-43214.herokuapp.com"
             })
         });
     
